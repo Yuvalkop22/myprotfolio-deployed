0330b6d93db9a08ca8b2780db2ee14501d660eaf
Yuvalkop22 <Kopelman050@gmail.com>
2023-03-30 16:09:40 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./App.css";
-
+import getinshape from './logos/getinshapelogo.png';
 export default function App() {
   return (
     <div className="portfolio">
@@ -15,22 +15,58 @@ export default function App() {
         <div className="project-cards">
           {/* Replace these placeholders with your actual projects */}
           <div className="project-card">
-            <img src="https://via.placeholder.com/300" alt="Project 1" />
-            <h3>Project 1</h3>
-            <p>A brief description of Project 1.</p>
-            <a href="#">Learn more</a>
+            <img src={getinshape} alt="Project 1" />
+            <h3>GETIN Shape</h3>
+            <p>Android application based on Java</p>
+            <div className="project-links">
+              <a href="https://github.com/Yuvalkop22/GETIN-shape-Kopelman">Github</a>
+              <a href="#">More Info</a>
+            </div>
           </div>
           <div className="project-card">
             <img src="https://via.placeholder.com/300" alt="Project 2" />
-            <h3>Project 2</h3>
-            <p>A brief description of Project 2.</p>
-            <a href="#">Learn more</a>
+            <h3>Yad-2</h3>
+            <p>Android + ROOM local database</p>
+            <div className="project-links">
+              <a href="https://github.com/Yuvalkop22/Yad2-Application">GitHub</a>
+              <a href="#">More Info</a>
+            </div>
           </div>
           <div className="project-card">
             <img src="https://via.placeholder.com/300" alt="Project 3" />
-            <h3>Project 3</h3>
-            <p>A brief description of Project 3.</p>
-            <a href="#">Learn more</a>
+            <h3>Flight Simulator</h3>
+            <p>Java project of flight simulator</p>
+            <div className="project-links">
+              <a href="https://github.com/PTM2-flight-simulator-team">GitHub</a>
+              <a href="#">More Info</a>
+            </div>
+          </div>
+          <div className="project-card">
+            <img src="https://via.placeholder.com/300" alt="Project 3" />
+            <h3>Web Clothes Store</h3>
+            <p>Web application - MERN + Scraping</p>
+            <div className="project-links">
+              <a href="https://github.com/Web-Store-Final-Project/react_final_project">GitHub</a>
+              <a href="#">More Info</a>
+            </div>
+          </div>
+          <div className="project-card">
+            <img src="https://via.placeholder.com/300" alt="Project 3" />
+            <h3>Gelato</h3>
+            <p>Web application - JQUERY, NodeJS</p>
+            <div className="project-links">
+              <a href="https://github.com/Yuvalkop22/Gelato">GitHub</a>
+              <a href="#">More Info</a>
+            </div>
+          </div>
+          <div className="project-card">
+            <img src="https://via.placeholder.com/300" alt="Project 3" />
+            <h3>Web Store</h3>
+            <p>Webpack Project</p>
+            <div className="project-links">
+              <a href="https://github.com/Yuvalkop22/store-webpack">GitHub</a>
+              <a href="#">More Info</a>
+            </div>
           </div>
         </div>
       </section>
@@ -40,7 +76,6 @@ export default function App() {
       </section>
       <section className="contact">
         <h2>Contact Me</h2>
-        {/* Replace these placeholders with your actual contact information */}
         <p>Email: Kopelman050@gmail.com</p>
         <p>Phone: 050-9516515</p>
         <p>Github: <a href="https://github.com/Yuvalkop22">YuvalKop22</a></p>
