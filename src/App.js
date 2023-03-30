04c4e76dbd01a4900ff9cd4cc2fa316616922874
Yuvalkop22 <Kopelman050@gmail.com>
2023-03-31 00:26:54 +0300

    FIXES

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import "./App.css";
 import getinshape from './logos/getinshapelogo.png';
+import webpack from './logos/webpack.png';
+import webstoremern from './logos/webstoremern.png';
+import gelato from './logos/gelato.jpg';
+import ptm from './logos/ptm.png';
+import bank from './logos/bank.jpg';
+import unit from './logos/8200unit.png';
 export default function App() {
   return (
     <div className="portfolio">
@@ -24,7 +30,7 @@ export default function App() {
             </div>
           </div>
           <div className="project-card">
-            <img src="https://via.placeholder.com/300" alt="Project 2" />
+            <img src="https://1000logos.net/wp-content/uploads/2016/10/Android-Logo-640x400.png" alt="Project 2" />
             <h3>Yad-2</h3>
             <p>Android + ROOM local database</p>
             <div className="project-links">
@@ -33,7 +39,7 @@ export default function App() {
             </div>
           </div>
           <div className="project-card">
-            <img src="https://via.placeholder.com/300" alt="Project 3" />
+            <img src={ptm} alt="Project 3" />
             <h3>Flight Simulator</h3>
             <p>Java project of flight simulator</p>
             <div className="project-links">
@@ -42,7 +48,7 @@ export default function App() {
             </div>
           </div>
           <div className="project-card">
-            <img src="https://via.placeholder.com/300" alt="Project 3" />
+            <img src={webstoremern} alt="Project 3" />
             <h3>Web Clothes Store</h3>
             <p>Web application - MERN + Scraping</p>
             <div className="project-links">
@@ -51,7 +57,7 @@ export default function App() {
             </div>
           </div>
           <div className="project-card">
-            <img src="https://via.placeholder.com/300" alt="Project 3" />
+            <img src={gelato} alt="Project 3" />
             <h3>Gelato</h3>
             <p>Web application - JQUERY, NodeJS</p>
             <div className="project-links">
@@ -60,7 +66,7 @@ export default function App() {
             </div>
           </div>
           <div className="project-card">
-            <img src="https://via.placeholder.com/300" alt="Project 3" />
+            <img src={webpack} alt="Project 3" />
             <h3>Web Store</h3>
             <p>Webpack Project</p>
             <div className="project-links">
@@ -72,7 +78,25 @@ export default function App() {
       </section>
       <section className="about">
         <h2>About Me</h2>
-        <p>Write a brief introduction about yourself and your skills here.</p>
+        <h3>Skills</h3>
+        <p>C,C++,C#,Java,Python</p>
+        <p>Web development - React, Angular 12, MongoDB, NodeJS, Express, Rest API</p>
+        <p>Android development - Java, SQL, Firebase</p>
+        <p>Other - Git, JIRA</p>
+        <h3>Military Service</h3>
+        <div className="mil-wrapper">
+          <p>8200 unit</p>
+          <img src={unit} className="unitimg" alt="img"/>
+        </div>
+        <p>Service at the Intelligence Corps, 8200 Unit.
+          Teaching and commanding of several courses in Arabic in 8200 Unit.</p>
+        <h3>Professional Experience</h3>
+        <div className="bank-wrapper">
+          <p>Bank of Israel</p>
+          <img src={bank} className="bankimg" alt="bank"/>
+        </div>
+        <p>Internship as a Full-Stack developer + DevOps "as a service".
+        Experienced with .NET, Angular 12 and Azure DevOps</p>
       </section>
       <section className="contact">
         <h2>Contact Me</h2>
